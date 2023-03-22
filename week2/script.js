7858b0976266708299d87a6d83d2b857dd83cc53
Valentyn Genkin <genkin.valentyn@gmail.com>
2023-03-22 11:37:38 +0100

    finished task 4

--- week2/script.js
+++ week2/script.js
@@ -63,7 +63,44 @@ while (item < 4) {
 
 // 4 task
 
+/**
+ * Keep track of which books you read and which books you want to read!
+ *  
+ * Follow the steps:
+ *  Declare a variable that holds an array of 3 objects, where each object describes a book and has properties for the title (string), author (string), and alreadyRead (boolean indicating if you read it yet).
+ *  Loop through the array of books.
+ *  For each book, log the book title and book author like so: "The Hobbit by J.R.R. Tolkien".
+ *  Create a conditional statement to change the log depending on whether you read it yet or not. If you read it, log a string like You already read "The Hobbit" right after the log of the book details
+ *  If you haven't read it log a string like You still need to read "The Lord of the Rings"
+ */
+
+const myBooks = [
+  {
+    author: 'Author 1',
+    title: 'Book 1',
+    read: true 
+  },
+  {
+    author: 'Author 2',
+    title: 'Book 2',
+    read: true 
+  },
+  {
+    author: 'Author 3',
+    title: 'Book 3',
+    read: false 
+  }
+]
+
+for (i = 0; i < myBooks.length; i++){
+  if 
+    (myBooks[i].read === false){
+    console.log ('You still need to read ' + myBooks[i].title )
+  } else {
+    console.log ('You already read ' + myBooks[i].title)
+  }
+}
 
 
-
+// 5 task
 
